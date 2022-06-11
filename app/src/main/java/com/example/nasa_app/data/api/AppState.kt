6181ec291529095a36713d412ad54a3619d93226
max <rodionovmax90test@gmail.com>
2022-06-11 18:18:17 -0400

    deleted old files

--- app/src/main/java/com/example/nasa_app/data/api/AppState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.nasa_app.data.api
-
-sealed class AppState {
-    data class Success(val success: Any) : AppState()
-    data class Error(val error: Throwable) : AppState()
-    object Loading : AppState()
-}
